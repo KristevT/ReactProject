92e95bb70978df7a92b6acb88800bd462706d427
KristevT <timkristev.cloud@gmail.com>
2024-12-02 11:00:11 +0300

    feat: adds profile view page

--- src/pages/user-page/UserPage.jsx
+++ src/pages/user-page/UserPage.jsx
@@ -1,8 +1,23 @@
+import { useSelector } from 'react-redux';
+import { Grid, GridItem } from '@consta/uikit/Grid';
+import { Text } from '@consta/uikit/Text';
+
 const UserPage = function(){
+    const user = useSelector(state => state.user.user) 
     return (
-        <div>
-            Профиль
-        </div>
+        <Grid gap="s" cols={4}>
+            <GridItem row={5}>
+                <img src={user?.image} alt='avatar'/>
+            </GridItem>
+            <GridItem col={3}>
+                <Text>
+                    <p> <Text size="2xl" weight="semibold"> Профиль </Text> </p>
+                    <p> <b>Имя:ㅤㅤㅤ</b> {user?.firstName} </p>
+                    <p> <b>Фамилия:ㅤ</b> {user?.lastName} </p>
+                    <p> <b>Почта:ㅤㅤ</b> {user?.email} </p>
+                </Text>
+            </GridItem>
+        </Grid>
     )
 }
 
